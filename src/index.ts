9aecfa509676372815618370643492c52073d5c6
ngmthaq-bglobalcorp <thangnm@bglobalcorp.com>
2023-12-27 13:41:13 +0700

    Publish E2EE to Github

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,52 @@
-export type EncryptResponseType = {
-  kx: string;
-  ky: string;
-  kz: number;
-};
-
 export class E2EEGenerator {
+  protected vigenereKeyRegex = /[a-zA-Z]/gm;
+  protected keyRegex = /(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,})$/gm;
+
+  /**
+   * Check if the Character is letter or not
+   *
+   * @param {string} string character to check
+   * @return {RegExpMatchArray} An array with the character or null if isn't a letter
+   */
+  protected isLetter(string: string): RegExpMatchArray {
+    return string.length === 1 && string.match(/[a-zA-Z]/i);
+  }
+
+  /**
+   * Check if is Uppercase or Lowercase
+   *
+   * @param character
+   * @returns {boolean}
+   */
+  protected isUpperCase(character: string): boolean {
+    return character === character.toUpperCase();
+  }
+
   /**
    * Encryption with row fence cipher
    *
    * @param {string} plainText
    * @param {number} key
-   * @param {string} padding
-   * @returns {EncryptResponseType}
+   * @returns {string}
    */
-  public encrypt(plainText: string, key?: number, padding?: string): EncryptResponseType {
-    const minKey = 2;
-    const encodeText = encodeURI(plainText);
-    const encodeTextLength = encodeText.length;
-    if (!key) key = Math.floor(Math.random() * (encodeTextLength - minKey + 1)) + minKey;
-    if (key <= 1) throw new Error("Key must be greater than 1");
-    if (!padding) padding = "=";
-    const encodeTextArray = encodeText.split("");
+  public rowFenceEncrypt(plainText: string, key: number): string {
+    key = key > 1 ? key : 2;
+    const plainTextLength = plainText.length;
+    const padding = "=";
+    const plainTextArray = plainText.split("");
     const rows: string[][] = [];
     for (let i = 0; i < key; i++) {
       rows[i] = [];
     }
     for (let j = 0; j < key; j++) {
-      for (let k = 0; k < Math.ceil(encodeTextLength / key); k++) {
+      for (let k = 0; k < Math.ceil(plainTextLength / key); k++) {
         const position = key * k + j;
-        const text = encodeTextArray[position];
+        const text = plainTextArray[position];
         rows[j].push(text ? text : padding);
       }
     }
-    const encryptedText = btoa(btoa(rows.map((row) => row.join("")).join("")));
-    return { kx: encryptedText, ky: padding, kz: key };
+    const encryptedText = rows.map((row) => row.join("")).join("");
+    return encryptedText;
   }
 
   /**
@@ -41,15 +54,12 @@ export class E2EEGenerator {
    *
    * @param {string} encryptedText
    * @param {number} key
-   * @param {string} padding
    * @returns {string} plain text
    */
-  public decrypt(encryptedText: string, key: number, padding?: string): string {
+  public rowFenceDecrypt(encryptedText: string, key: number): string {
     try {
-      if (key <= 1) throw new Error("Key must be greater than 1");
-      if (!padding) padding = "=";
-      encryptedText = encryptedText.split(".")[0];
-      encryptedText = atob(atob(encryptedText));
+      key = key > 1 ? key : 2;
+      const padding = "=";
       const encryptedTextLength = encryptedText.length;
       const encryptedTextArray = encryptedText.split("");
       const columns = Math.round(encryptedTextLength / key);
@@ -72,10 +82,100 @@ export class E2EEGenerator {
         if (char !== padding) encodeTextArray.push(char);
       }
       const encodeText = encodeTextArray.reverse().join("");
-      return decodeURI(encodeText);
+      return encodeText;
     } catch {
       return encryptedText;
     }
+  }
+
+  /**
+   * Encrypt a Vigenere cipher
+   *
+   * @param {string} message string to be encrypted
+   * @param {string} key key for encrypt
+   * @return {string} encrypted string
+   */
+  public vigenereEncrypt(message: string, key: string): string {
+    let result = "";
+    for (let i = 0, j = 0; i < message.length; i++) {
+      const c = message.charAt(i);
+      if (this.isLetter(c)) {
+        if (this.isUpperCase(c)) {
+          result += String.fromCharCode(((c.charCodeAt(0) + key.toUpperCase().charCodeAt(j) - 2 * 65) % 26) + 65);
+        } else {
+          result += String.fromCharCode(((c.charCodeAt(0) + key.toLowerCase().charCodeAt(j) - 2 * 97) % 26) + 97);
+        }
+      } else {
+        result += c;
+      }
+      j = ++j % key.length;
+    }
+    return result;
+  }
+
+  /**
+   * Decrypt a Vigenere cipher
+   *
+   * @param {string} message string to be decrypted
+   * @param {string} key key for decrypt
+   * @return {string} decrypted string
+   */
+  public vigenereDecrypt(message: string, key: string): string {
+    try {
+      let result = "";
+      for (let i = 0, j = 0; i < message.length; i++) {
+        const c = message.charAt(i);
+        if (this.isLetter(c)) {
+          if (this.isUpperCase(c)) {
+            result += String.fromCharCode(90 - ((25 - (c.charCodeAt(0) - key.toUpperCase().charCodeAt(j))) % 26));
+          } else {
+            result += String.fromCharCode(122 - ((25 - (c.charCodeAt(0) - key.toLowerCase().charCodeAt(j))) % 26));
+          }
+        } else {
+          result += c;
+        }
+        j = ++j % key.length;
+      }
+      return result;
+    } catch {
+      return message;
+    }
+  }
+
+  /**
+   * Full encrypt
+   *
+   * @param {string} input
+   * @param {string} key
+   * @returns {string}
+   */
+  public encrypt(input: string, key: string): string {
+    if (!key) throw new Error("Key must not be empty");
+    if (key.match(this.keyRegex) === null) throw new Error("Key must contain number and letter and must be more than 6 characters");
+    if (isNaN(parseInt(key[0]))) throw new Error("First character of key must be in number");
+    const k0 = encodeURI(input);
+    const k1 = btoa(k0);
+    const k2 = this.vigenereEncrypt(k1, key.match(this.vigenereKeyRegex).join(""));
+    const k3 = this.rowFenceEncrypt(k2, parseInt(key[0]));
+    return k3;
+  }
+
+  /**
+   * Full decrypt
+   *
+   * @param {string} input
+   * @param {string} key
+   * @returns {string}
+   */
+  public decrypt(input: string, key: string): string {
+    if (!key) throw new Error("Key must not be empty");
+    if (key.match(this.keyRegex) === null) throw new Error("Key must contain number and letter and must be more than 6 characters");
+    if (isNaN(parseInt(key[0]))) throw new Error("First character of key must be in number");
+    const k0 = this.rowFenceDecrypt(input, parseInt(key[0]));
+    const k1 = this.vigenereDecrypt(k0, key.match(this.vigenereKeyRegex).join(""));
+    const k2 = atob(k1);
+    const k3 = decodeURI(k2);
+    return k3;
   }
 }
 
