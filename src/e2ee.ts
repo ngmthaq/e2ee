b0f4abfb715fc55e6d9cdee73172b6848242427a
ngmthaq-bglobalcorp <thangnm@bglobalcorp.com>
2023-12-27 11:00:36 +0700

    Publish E2EE to Github

---
+++ src/e2ee.ts
@@ -0,0 +1,3 @@
+import E2EE from "./index";
+
+window.E2EE = E2EE;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-class E2EE {
+export type EncryptResponseType = {
+  kx: string;
+  ky: string;
+  kz: number;
+};
+
+export default class E2EE {
   /**
    * Encryption with row fence cipher
    *
    * @param {string} plainText
    * @param {number} key
    * @param {string} padding
-   * @returns {string}
+   * @returns {EncryptResponseType}
    */
-  public encrypt(plainText: string, key?: number, padding?: string): string {
+  public encrypt(plainText: string, key?: number, padding?: string): EncryptResponseType {
     const minKey = 2;
     const encodeText = encodeURI(plainText);
     const encodeTextLength = encodeText.length;
@@ -16,7 +22,9 @@ class E2EE {
     if (!padding) padding = "=";
     const encodeTextArray = encodeText.split("");
     const rows: string[][] = [];
-    for (let i = 0; i < key; i++) rows[i] = [];
+    for (let i = 0; i < key; i++) {
+      rows[i] = [];
+    }
     for (let j = 0; j < key; j++) {
       for (let k = 0; k < Math.ceil(encodeTextLength / key); k++) {
         const position = key * k + j;
@@ -24,8 +32,8 @@ class E2EE {
         rows[j].push(text ? text : padding);
       }
     }
-    const encryptedText = btoa(rows.map((row) => row.join("")).join(""));
-    return [encryptedText, key, padding].join(".");
+    const encryptedText = btoa(btoa(rows.map((row) => row.join("")).join("")));
+    return { kx: encryptedText, ky: padding, kz: key };
   }
 
   /**
@@ -37,32 +45,36 @@ class E2EE {
    * @returns {string} plain text
    */
   public decrypt(encryptedText: string, key: number, padding?: string): string {
-    if (!padding) padding = "=";
-    encryptedText = atob(encryptedText);
-    const encryptedTextLength = encryptedText.length;
-    const encryptedTextArray = encryptedText.split("");
-    const columns = Math.round(encryptedTextLength / key);
-    const rows: string[][] = [];
-    const plainTextRows: string[][] = [];
-    for (let i = 0; i < key; i++) {
-      for (let j = 0; j < columns; j++) {
-        if (!rows[i]) rows[i] = [];
-        const position = i * columns + j;
-        rows[i].push(encryptedTextArray[position]);
+    try {
+      if (key <= 1) throw new Error("Key must be greater than 1");
+      if (!padding) padding = "=";
+      encryptedText = encryptedText.split(".")[0];
+      encryptedText = atob(atob(encryptedText));
+      const encryptedTextLength = encryptedText.length;
+      const encryptedTextArray = encryptedText.split("");
+      const columns = Math.round(encryptedTextLength / key);
+      const rows: string[][] = [];
+      const plainTextRows: string[][] = [];
+      for (let i = 0; i < key; i++) {
+        for (let j = 0; j < columns; j++) {
+          if (!rows[i]) rows[i] = [];
+          const position = i * columns + j;
+          rows[i].push(encryptedTextArray[position]);
+        }
       }
+      for (let p = 0; p < columns; p++) {
+        plainTextRows[p] = rows.map((row) => row[p]);
+      }
+      const decryptedText = plainTextRows.map((row) => row.join("")).join("");
+      const encodeTextArray = [];
+      for (let k = decryptedText.length; k > 0; k--) {
+        const char = decryptedText[k - 1];
+        if (char !== padding) encodeTextArray.push(char);
+      }
+      const encodeText = encodeTextArray.reverse().join("");
+      return decodeURI(encodeText);
+    } catch {
+      return encryptedText;
     }
-    for (let p = 0; p < columns; p++) {
-      plainTextRows[p] = rows.map((row) => row[p]);
-    }
-    const decryptedText = plainTextRows.map((row) => row.join("")).join("");
-    const encodeTextArray = [];
-    for (let k = decryptedText.length; k > 0; k--) {
-      const char = decryptedText[k - 1];
-      if (char !== padding) encodeTextArray.push(char);
-    }
-    const encodeText = encodeTextArray.reverse().join("");
-    return decodeURI(encodeText);
   }
 }
-
-export default E2EE;
